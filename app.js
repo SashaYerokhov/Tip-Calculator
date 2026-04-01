2d80fb9a4945d90f919307549272c3eb179e6091
SashaYerokhov <Ghost0737@gmail.com>
2026-04-01 16:25:29 +0300

    Beginer JavaScript-files

---
+++ app.js
@@ -0,0 +1,8 @@
+const tipsBill = document.querySelector('#tipsBill');
+const btnPercent = document.querySelectorAll('.btn');
+const custom = document.querySelector('#custom');
+const tipsPerson = document.querySelector('#tipsPerson');
+const amountPrice = document.querySelector('amount-price');
+const totalPrice = document.querySelector('.total-price');
+const resetBtn = document.querySelector('.reset');
+// console.log(tipsBill, btnPercent, custom, tipsPerson, amountPrice, totalPrice, resetBtn);
